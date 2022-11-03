373696c28fb62388b4b19ca5ddd3881829d88798
ramGit22 <karki_ram@hotmail.com>
2022-11-03 15:54:50 +0200

    authorization route

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,6 @@
 import express from "express";
+import helmet from "helmet";
+import morgan from "morgan";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import userRoute from "./routes/users.js";
@@ -9,27 +11,29 @@ dotenv.config();
 const connect = async () => {
   try {
     await mongoose.connect(process.env.CONNECTION_URI);
-    console.log("connected to mongoDB")
+    console.log("connected to mongoDB");
   } catch (error) {
     throw error;
   }
-}
+};
 
-mongoose.connection.on("disconnected", ()=> {
-  console.log("mongoDB disconnected!")
-})
+mongoose.connection.on("disconnected", () => {
+  console.log("mongoDB disconnected!");
+});
 
-mongoose.connection.on("connected", ()=>{
-  console.log("mongoDB connected")
-})
+mongoose.connection.on("connected", () => {
+  console.log("mongoDB connected");
+});
 
-app.get("/", (req, res)=>{
-  res.send("hello world")
-})
-
-app.use("/api/auth", authRoute)
-app.use("/api/users", userRoute)
-app.listen(8800, ()=>{
-  connect()
-  console.log("connected to backend")
-})
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.listen(8800, () => {
+  connect();
+  console.log("connected to backend");
+});
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,8 +1,65 @@
 import express from "express";
+import User from "../models/User.js";
+import bcrypt from "bcrypt";
 const router = express.Router();
 
-router.get("/", (req,res)=>{
-    res.send("This is a auth route")
-})
+//Register endpoint
+router.post("/register", async (req, res) => {
+  try {
+    //generate new password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+    //create new user
+    const newUser = new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: hashedPassword,
+    });
+
+    //save user and respond
+    const user = await newUser.save();
+    res.send(user);
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+// //Login endpoint
+// router.post("/login", async (req, res) => {
+//   try {
+//     //check if email exists
+//     const user = await User.findOne({ email: req.body.email });
+//     !user && res.send("user not found");
+
+//     //validate password
+//     const validPassword = await bcrypt.compare(
+//       req.body.password,
+//       user.password
+//     );
+//     !validPassword && res.send("Wrong password");
+
+//     res.status(200).json(user);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// });
+
+//LOGIN
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    !user && res.status(404).json("user not found");
+    const validPassword = await bcrypt.compare(
+      { password: req.body.password },
+      user.password
+    );
+    !validPassword && res.status(400).json("wrong password");
+
+    res.json(user);
+  } catch (err) {
+    console.log(err);
+  }
+});
 
 export default router;
