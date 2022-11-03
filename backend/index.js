4c1ffd93b3872af21f5ce6881653d2aa12320cd9
Ramesh <karki_ram@hotmail.com>
2022-11-03 10:54:24 +0200

    database connection and routing

---
+++ backend/index.js
@@ -0,0 +1,35 @@
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import userRoute from "./routes/users.js";
+import authRoute from "./routes/auth.js";
+const app = express();
+dotenv.config();
+
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.CONNECTION_URI);
+    console.log("connected to mongoDB")
+  } catch (error) {
+    throw error;
+  }
+}
+
+mongoose.connection.on("disconnected", ()=> {
+  console.log("mongoDB disconnected!")
+})
+
+mongoose.connection.on("connected", ()=>{
+  console.log("mongoDB connected")
+})
+
+app.get("/", (req, res)=>{
+  res.send("hello world")
+})
+
+app.use("/api/auth", authRoute)
+app.use("/api/users", userRoute)
+app.listen(8800, ()=>{
+  connect()
+  console.log("connected to backend")
+})
