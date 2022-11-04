17c0f488ad4df01ceca97c84ecb9e015e57ca910
Ramesh <karki_ram@hotmail.com>
2022-11-04 22:28:55 +0200

    login endpoint

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,7 +2,7 @@ import express from "express";
 import User from "../models/User.js";
 import bcrypt from "bcrypt";
 const router = express.Router();
-
+  
 //Register endpoint
 router.post("/register", async (req, res) => {
   try {
@@ -25,41 +25,20 @@ router.post("/register", async (req, res) => {
   }
 });
 
-// //Login endpoint
-// router.post("/login", async (req, res) => {
-//   try {
-//     //check if email exists
-//     const user = await User.findOne({ email: req.body.email });
-//     !user && res.send("user not found");
-
-//     //validate password
-//     const validPassword = await bcrypt.compare(
-//       req.body.password,
-//       user.password
-//     );
-//     !validPassword && res.send("Wrong password");
-
-//     res.status(200).json(user);
-//   } catch (error) {
-//     console.log(error);
-//   }
-// });
-
-//LOGIN
+//Login endpoint
 router.post("/login", async (req, res) => {
   try {
     const user = await User.findOne({ email: req.body.email });
     !user && res.status(404).json("user not found");
+
     const validPassword = await bcrypt.compare(
-      { password: req.body.password },
-      user.password
+      req.body.password, //password from the request
+      user.password //password from the database
     );
     !validPassword && res.status(400).json("wrong password");
-
-    res.json(user);
+    res.send(user);
   } catch (err) {
     console.log(err);
   }
 });
-
 export default router;
